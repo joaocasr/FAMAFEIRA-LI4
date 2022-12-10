1d210a62bf36b6a569306370fc3b4bce3b6d5907
joaocasr <castrojoao839@gmail.com>
2022-12-10 22:47:18 +0000

    organizacao estrutural do projeto, gestao de entidades, pagamento de produtos e consulta de vendas

---
+++ FamaFeira/FamaFeira/Models/Feira.cs
@@ -0,0 +1,28 @@
+﻿namespace FamaFeira.Models
+{
+    public class Feira
+    {
+
+        public Feira(string tipo,string designacao, string localizacao, string imagem, string data)
+        {
+            this.tipo = tipo;
+            this.designacao = designacao;   
+            this.localizacao = localizacao;
+            this.imagem = imagem;
+            this.data = data;
+
+        }
+
+        public string tipo { get; set; }
+        public string designacao { get; set; }
+        public string localizacao { get; set; }
+        public string imagem { get; set; }
+        public string data { get; set; }
+
+        public String getTipo()
+        {
+            return this.tipo;
+        }
+
+    }
+}
