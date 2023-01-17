0a656f15c7180c92bd2810bee21e81045a953ab3
joaocasr <castrojoao839@gmail.com>
2023-01-17 22:21:21 +0000

    atualizacao

--- FamaFeira/FamaFeira/Models/DAL/DALconfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FamaFeira.Models.DAL
-{
-    public class DALconfig
-    {
-        public static string connectionstring = @"Data Source = LAPTOP-8C5JF0N5; Initial Catalog = FamaFeiradb; Integrated Security = True;";
-    }
-}
